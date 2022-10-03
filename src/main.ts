98c30cd003d9cae75746c1264011758fa7b9cf87
flingyp <1213563369@qq.com>
2022-10-03 22:43:42 +0800

    feat: 设置应用路由前缀

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // 设置应用路由前缀
+  app.setGlobalPrefix('starter');
+
   await app.listen(3000);
 }
 bootstrap();
