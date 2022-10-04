56485c8ea4becab668b6cb9e1f1c4223b7e20b5e
flingyp <1213563369@qq.com>
2022-10-04 08:52:03 +0800

    feat: 日志中间件打印内容补充

--- src/middleware/logger.middleware.ts
+++ src/middleware/logger.middleware.ts
@@ -2,13 +2,18 @@ import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 
 const logFormatMessage = (req: Request) => {
-  return `Method: ${req.method} \n Request original url: ${req.url} \n IP: ${req.ip} \n Status code: ${req.statusCode} \n`;
+  const logMessage = `Method: ${req.method} \n Request original url: ${
+    req.url
+  } \n IP: ${req.ip} \n Query params: ${JSON.stringify(
+    req.query,
+  )} \n Body params: ${JSON.stringify(req.body)} \n`;
+  console.log(logMessage);
 };
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    console.log(logFormatMessage(req));
+    logFormatMessage(req);
     next();
   }
 }
